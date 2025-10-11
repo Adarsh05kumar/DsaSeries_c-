cdab1d4de14f5391e949efc77e71fa7d56c4136a
Adarsh05kumar <adarshprajapati985@gmail.com>
2025-10-11 11:14:08 +0530

    Time: 2 ms (6.03%) | Memory: 8.2 MB (52.30%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(auto x:word){
+            if(x>='A'&&  x<='Z'){
+                count++;
+            }
+        }
+        if(count==word.size()){
+            return true;
+        }if(count==0){
+            return true;
+        }if(word[0]>='A' && word[0]<='Z' && count==1){
+            return true;
+        }
+        return false;
+        
+    }
+};
